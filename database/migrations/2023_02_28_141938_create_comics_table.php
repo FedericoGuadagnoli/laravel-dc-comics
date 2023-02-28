1677f0fdcf200a5332ae9fc6386d0a93aacf8913
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-28 16:51:39 +0100

    create  table structure & table data

--- database/migrations/2023_02_28_141938_create_comics_table.php
+++ database/migrations/2023_02_28_141938_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->unique();
+            $table->text('description');
+            $table->text('thumb')->nullable();
+            $table->string('price', 10);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 50)->nullable();
+            $table->json('artists');
+            $table->json('writers');
             $table->timestamps();
         });
     }

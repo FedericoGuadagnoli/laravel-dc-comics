dde6a2ed6dd2305f699f07928d97db1a8f19bb21
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-28 20:36:09 +0100

    create route show in tag a in index.blade

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -12,7 +12,7 @@
             @foreach ($comics as $comic )               
             <div class="card">
                 <figure>
-                    <a href="">
+                    <a href="{{ route('comics.show', $comic->id)}}">
                         <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
                     </a>
                 </figure>

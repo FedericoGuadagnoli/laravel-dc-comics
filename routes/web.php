77474b008533e46245756c5f56d2499b548eacd5
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-03-01 16:05:44 +0100

    create route store

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.c
 
 Route::get('/comics/{id}', [ComicController::class, 'show'])->name('comics.show');
 
+Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
+
 
 Route::get('/characters', function () {
     return view('characters');

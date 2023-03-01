f7bd1cf9f33b07a34a975003bf103b35c672f619
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-03-01 15:22:36 +0100

    create "create.blade" with template & style

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('title', 'Crea comic')
+    
+@section('main-content')
+<h1>Crea comic</h1>
+<a href="{{ route('comics.index')}}">Indietro</a>
+<form action="{{route('comics.store')}}" method="POST" class="form-control">
+    <div class="left-form">
+        <label for="title">Nome fumetto</label>
+        <input type="text" id="title" name="title" placeholder="Nome del fumetto">
+    
+        <label for="description">Descrizione</label>
+        <textarea name="description" id="description" cols="50" placeholder="Descrizione del fumetto"></textarea>
+        
+        <label for="price">Prezzo</label>
+        <input type="text" id="price" name="price" placeholder="Prezzo del fumetto">
+    </div>
+    <div class="right-form">
+        <label for="thumb">Immagine fumetto</label>
+        <input type="url" id="thumb" name="thumb" placeholder="Immagine del fumetto">
+
+        <label for="series">Serie</label>
+        <input type="text" id="series" name="series" placeholder="Serie">
+    
+        <label for="sale_date">Nome fumetto</label>
+        <input type="sale_date" id="sale_date" name="sale_date" placeholder="Data di commercializzazione">
+
+        <label for="type">Tipo fumetto</label>
+        <input type="type" id="type" name="type" placeholder="Tipo di fumetto">
+    </div>
+    <div class="btn">
+        <button type="submit">Salva</button>
+    </div>
+</form>
+@endsection
